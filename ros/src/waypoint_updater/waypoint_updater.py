e6c486ecc0a0a397018e0e70859247cc72ec35b1
Reavenk <reavenk@gmail.com>
2018-03-10 00:42:49 -0600

    Slowdown on sharp turn prototype

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -70,11 +70,27 @@ class WaypointUpdater(object):
         if stopidx == highval:
             rospy.logdebug("Gauss - Generated forward waypoints")
             for wpt in range(index, index+LOOKAHEAD_WPS):
-                self.set_waypoint_velocity(self.base_waypoints_msg.waypoints, wpt % self.waypt_count, self.targetvel)
+                # Depending on how curvy the road is ahead, slow down. 
+                # It's usually subtle, but it's dramatically noticeable
+                # on the sharper turns.
+                #
+                # We may want to find an intelligent way to calculate
+                # the skip and ct parameters- such as based off the 
+                # current speed or even PID error.
+                #
+                # It would also need a strategy on how to be combined with 
+                # other velocity-considerations, such as 
+                wptMod = wpt % self.waypt_count
+                straight = self.check_waypoint_straight_factor(self.waypoints_positions, index, 20, 10)
+                straight = math.pow(straight, 1.5) # Exaggerate/curve it
+                self.set_waypoint_velocity(self.base_waypoints_msg.waypoints, wptMod, self.targetvel * straight)
         else:
             rospy.logdebug("Gauss - Generated stop waypoints") #(Not really)
             for wpt in range(index, index+LOOKAHEAD_WPS):
-                self.set_waypoint_velocity(self.base_waypoints_msg.waypoints, wpt % self.waypt_count, self.targetvel)
+                wptMod = wpt % self.waypt_count
+                straight = self.check_waypoint_straight_factor(self.waypoints_positions, index, 20, 10)
+                straight = math.pow(straight, 1.5) # Exagerate/curve it
+                self.set_waypoint_velocity(self.base_waypoints_msg.waypoints, wptMod, self.targetvel * straight)
 
         waypoints_sliced = self.base_waypoints_msg.waypoints[index:index+LOOKAHEAD_WPS]
         output_msg = Lane()
@@ -83,6 +99,11 @@ class WaypointUpdater(object):
 
         rospy.loginfo("Gauss - Publishing Waypoints of length: " + str(len(output_msg.waypoints)))
         self.final_waypoints_pub.publish(output_msg)
+		
+        straight = self.check_waypoint_straight_factor(self.waypoints_positions, index, 20, 10)
+        
+        
+        #rospy.logerr(straight)
 
     def pose_cb(self, msg):
         # @done: Implement
@@ -127,6 +148,42 @@ class WaypointUpdater(object):
 
     def kmph2mps(self, velocity_kmph):
         return (velocity_kmph * 1000.) / (60. * 60.)
+		
+    '''
+    Calculate a "straightness factor" by sparsely checking some 
+    uniformly spaced gradients ahead of a specified waypoint and
+    combining their various values.
+    '''
+    def check_waypoint_straight_factor(self, waypoint_points, start, skip, ct):
+        factor = 1.0
+        wpt = start
+        for i in range(ct):
+            wptNext = (wpt + skip + 1)%self.waypt_count
+            # get sample points
+            ptprv = waypoint_points[((wpt - skip - 1) + self.waypt_count) % self.waypt_count]
+            ptcur = waypoint_points[wpt]
+            ptnxt = waypoint_points[wptNext]
+            # get vectors
+            veccur = [ptcur[0] - ptprv[0], ptcur[1] - ptprv[1]]
+            vecnxt = [ptnxt[0] - ptcur[0], ptnxt[1] - ptcur[1]]
+            #normalize and dot product
+            cn = math.sqrt(veccur[0] * veccur[0] + veccur[1] * veccur[1])
+            nn = math.sqrt(vecnxt[0] * vecnxt[0] + vecnxt[1] * vecnxt[1])
+            veccur = [veccur[0] / cn, veccur[1]/cn]
+            vecnxt = [vecnxt[0] / nn, vecnxt[1]/nn]
+            dot = veccur[0] * vecnxt[0] + veccur[1] * vecnxt[1] # Dotproduct to get the cos of their angles
+            # remap to [0.0, 1.0]
+            # We use asin instead of acos because we want a high value if
+            # the angles are the same, similar to the original dot product
+            # value - we can't use the original dot product value though
+            # because it's too sensitive in the section that we don't want
+            # it to be (actual angles are more effective than cos values)
+            subfactor = math.asin(min(max(dot, 0.0), 1.0))/math.pi * 2      # Check min and max or else risk a function domain exception
+            #apply - note that tuning parameters skip and ct to get a sane
+            # calculated factor value is kind of an art.
+            factor *= subfactor 
+            wpt = wptNext
+        return factor
 
 if __name__ == '__main__':
     try:
