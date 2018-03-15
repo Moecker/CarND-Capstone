7c9141eb1c41a8ea44a59ff0c1ca86d5575bc76f
Hiroshi Ichiki <Hiroshi.Ichiki@sony.com>
2018-03-15 18:15:00 +0900

    new branch for Traffic Light Classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,71 @@
+import pickle
+import numpy as np
+from PIL import Image
+
+import keras
+from keras.applications.imagenet_utils import preprocess_input
+from keras.preprocessing import image
+from scipy.misc import imresize
+
+from ssd import SSD300
+from ssd_utils import BBoxUtility
+
 from styx_msgs.msg import TrafficLight
+
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        NUM_CLASSES = 3 + 1
+        input_shape = (300, 300, 3)
 
-    def get_classification(self, image):
+        # "prior boxes" in the paper
+        priors = pickle.load(open('/home/student/.ros/prior_boxes_ssd300.pkl', 'rb'))
+        self.bbox_util = BBoxUtility(NUM_CLASSES, priors)
+        self.model = SSD300(input_shape, num_classes=NUM_CLASSES)
+        self.model.load_weights('/home/student/.ros/weights.180314.hdf5', by_name=True)
+
+    def get_classification(self, img):
         """Determines the color of the traffic light in the image
 
         Args:
-            image (cv::Mat): image containing the traffic light
+            img (cv::Mat): image containing the traffic light
+            assumed 3D numpy.array (800, 600, 3)
+            bgr8: CV_8UC3, color image with blue-green-red color order
 
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+        pilImg = Image.fromarray(np.uint8(img)).resize((300, 300))
+        img = np.array(pilImg)
+        img = image.img_to_array(img)
+        inputs = np.reshape(img, (1, 300, 300, 3))  # 'inputs' expects this size
+
+        inputs = preprocess_input(np.array(inputs))
+        preds = self.model.predict(inputs, batch_size=1, verbose=0)
+        results = self.bbox_util.detection_out(preds)
+
+        det_label = results[0][:, 0]
+        det_conf = results[0][:, 1]
+
+        # Get detections with confidence >= 0.8
+        top_indices = [j for j, conf in enumerate(det_conf) if conf >= 0.8]
+        top_conf = det_conf[top_indices]
+        top_label_indices = det_label[top_indices].tolist()
+        if top_label_indices == []:
+            return TrafficLight.UNKNOWN, 0, 0, 0, 0, 0
+        
+        # assume only one signal detected
+        label = int(top_label_indices[0])
+        if label == 0:
+            return TrafficLight.UNKNOWN
+        elif label == 1:
+            return TrafficLight.RED
+        elif label == 2:
+            return TrafficLight.YELLOW
+        elif label == 3:
+            return TrafficLight.GREEN
+        else:
+            return TrafficLight.UNKNOWN
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -129,8 +129,8 @@ class TLDetector(object):
 
         #Get classification
         #TODO: remove the `return light.state` statement and uncomment the following line
-        #return self.light_classifier.get_classification(cv_image)
-        return light.state
+        return self.light_classifier.get_classification(cv_image)
+        #return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
