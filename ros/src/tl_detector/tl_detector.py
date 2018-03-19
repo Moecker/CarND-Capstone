e3210922714a14f66eb7ca44264609e238f70d50
David Obando <daobando@microsoft.com>
2018-03-18 19:25:55 -0700

    Updating simulator weights file

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -149,7 +149,7 @@ class TLDetector(object):
             closest_stop_line_index = self.get_closest_waypoint(self.pose.pose.position, self.stop_line_positions)
             closest_stop_line = self.stop_line_positions[closest_stop_line_index]
             light_wp = self.get_closest_waypoint(Point(closest_stop_line[0], closest_stop_line[1], 0))
-            if car_position < light_wp and light_wp - car_position < 100: # assume visibility is 100 meters
+            if car_position < light_wp and light_wp - car_position < 150: # assume visibility is 150 meters
                 for real_light in self.lights:
                     light_position = real_light.pose.pose.position
                     light_x_approx = abs(light_position.x - closest_stop_line[0]) < LIGHT_LOCATION_THRESHOLD
